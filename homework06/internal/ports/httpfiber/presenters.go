61e1757a5b75efb0b2ce9b6a660c5d15d5dace4e
Матвей Попов <evgeniyprohorov24@gmail.com>
2023-07-17 12:07:52 +0300

    Initial commit

---
+++ homework06/internal/ports/httpfiber/presenters.go
@@ -0,0 +1,52 @@
+package httpfiber
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"homework6/internal/ads"
+)
+
+type createAdRequest struct {
+	Title  string `json:"title"`
+	Text   string `json:"text"`
+	UserID int64  `json:"user_id"`
+}
+
+type adResponse struct {
+	ID        int64  `json:"id"`
+	Title     string `json:"title"`
+	Text      string `json:"text"`
+	AuthorID  int64  `json:"author_id"`
+	Published bool   `json:"published"`
+}
+
+type changeAdStatusRequest struct {
+	Published bool  `json:"published"`
+	UserID    int64 `json:"user_id"`
+}
+
+type updateAdRequest struct {
+	Title  string `json:"title"`
+	Text   string `json:"text"`
+	UserID int64  `json:"user_id"`
+}
+
+func AdSuccessResponse(ad *ads.Ad) *fiber.Map {
+	return &fiber.Map{
+		"data": adResponse{
+			ID:        ad.ID,
+			Title:     ad.Title,
+			Text:      ad.Text,
+			AuthorID:  ad.AuthorID,
+			Published: ad.Published,
+		},
+		"error": nil,
+	}
+}
+
+func AdErrorResponse(err error) *fiber.Map {
+	return &fiber.Map{
+		"data":  nil,
+		"error": err.Error(),
+	}
+}
